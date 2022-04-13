8b0b76f3bbc09c011a75b95e25b289931f589040
robin <robin.prillwitz@gmail.com>
2022-04-13 15:21:49 +0200

    Update plot.py

--- tools/plot.py
+++ tools/plot.py
@@ -1,13 +1,39 @@
 import sys
+import math
 import select
 import serial
 import time
+import atexit
 import keyboard
 import argparse
 import threading
 import datetime
 
 VERSION = "v1.0.0"
+ser = None
+
+def availabeBuffer():
+    while 1:
+        reminaingBuffer = 0
+        ser.flushInput()
+        ser.flushInput()
+
+        ser.write(bytes("\033.B\r\n", "utf-8"))
+        time.sleep(0.5)
+        raw = ser.read_until("\r", 5)
+        print(raw)
+
+        try:
+            if raw:
+                reminaingBuffer, _ = raw.decode("utf-8").split("\r")
+            else:
+                reminaingBuffer = -1
+        except:
+            reminaingBuffer = -2
+
+
+        print("REM", reminaingBuffer)
+    return int(reminaingBuffer)
 
 class Job(threading.Thread):
 
@@ -23,28 +49,34 @@ class Job(threading.Thread):
         self.delay = delay
 
     def run(self):
+        global ser
+
         print("opening file", self.file)
-        line_list = list()
+        lines = ""
         with open(self.file,"r") as f:
-            line_list = [l.strip() for l in list(f.readlines())]
+            lines = f.read()
+        command_list = lines.split(";")
+
+        line_list = list()
+        for i in range(len(command_list)):
+            line_list.append(command_list[i] + ";")
+        line_list = [f.strip() for f in line_list]
 
         print("opening serial port", self.port)
         ser = serial.Serial(self.port, 9600, timeout=0.5)
 
-        i = 0;
-        while(i < len(line_list)):
+        i = 0
+        print("LEN", len(line_list))
+        while i < len(line_list):
+            print(i, line_list[i])
             ser.write(bytes(line_list[i], "utf-8"))
-
-            if(i % 50 == 0):
-                time_remaining = int(round(((len(line_list) - i) * self.delay), 0))
-                tm = datetime.datetime.fromtimestamp(time_remaining)
-                progress = round(i / len(line_list) * 100, 2)
-                print(f"{i}/{len(line_list)} {tm.strftime('%M:%S')} {progress}%")
+            time.sleep(len(line_list[i]) * 0.01)
 
             i = i + 1;
-            time.sleep(self.delay)
+
             if(self.__running.isSet()):
                 self.__flag.wait()
+
 
         ser.close()
         self.stop()
@@ -56,8 +88,22 @@ class Job(threading.Thread):
         self.__flag.set()
 
     def stop(self):
+        print("stopping")
         self.__flag.set()
         self.__running.clear()
+
+def exitHandler():
+    global ser
+    try:
+        ser.write(bytes("\033.J\r\n", "utf-8"))
+        ser.write(bytes("\033.K\r\n", "utf-8"))
+        ser.write(bytes("\033.Z\r\n", "utf-8"))
+        ser.close()
+    except: pass
+    print("Aborting")
+    return 0
+
+atexit.register(exitHandler)
 
 parser = argparse.ArgumentParser(description="Roland DXY-1150 Plotter utility")
 parser.add_argument('-p', '--port',
@@ -77,7 +123,7 @@ parser.add_argument('-d', '--delay',
                     dest='delay',
                     help='Command delay',
                     type=float,
-                    default=0.2
+                    default=0.05
                     )
 parser.add_argument('-v', '--version',
                     action='store_true',
@@ -100,7 +146,10 @@ if(args.get("port", None) is None):
 a = Job(args.get("port"), args.get("file"), args.get("delay"))
 a.start()
 
-time.sleep(0.2)
+time.sleep(0.5)
+
+# b = threading.Thread(target=availabeBuffer)
+# b.run()
 
 print("\ntype \'p\' to pause and \'r\' to resume\n")
 while a.is_alive():
