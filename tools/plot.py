7643f29328506ff30462f72444e899ba19c63501
robin <robin.prillwitz@gmail.com>
2023-01-31 18:59:49 +0100

    various changes to plot py

--- tools/plot.py
+++ tools/plot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import math
 import select
@@ -11,33 +13,35 @@ import datetime
 
 VERSION = "v1.0.0"
 ser = None
+success = False
 
 def availabeBuffer():
-    while 1:
-        reminaingBuffer = 0
-        ser.flushInput()
-        ser.flushInput()
+    # while 1:
+    reminaingBuffer = 0
 
-        ser.write(bytes("\033.B\r\n", "utf-8"))
-        time.sleep(0.5)
-        raw = ser.read_until("\r", 5)
-        print(raw)
+    ser.write(bytes("\033.B;", "utf-8"))
+    # time.sleep(0.5)
+    raw = ser.read_until("\r", 5)
+    print("RAW", raw)
 
-        try:
-            if raw:
-                reminaingBuffer, _ = raw.decode("utf-8").split("\r")
-            else:
-                reminaingBuffer = -1
-        except:
-            reminaingBuffer = -2
+    # print("BUFFER", ser.out_waiting, ser.in_waiting)
 
+    try:
+        if raw:
+            reminaingBuffer, _ = raw.decode("utf-8").split("\r")
+        else:
+            reminaingBuffer = -1
+    except:
+        reminaingBuffer = -2
 
-        print("REM", reminaingBuffer)
-    return int(reminaingBuffer)
+    reminaingBuffer = int(reminaingBuffer)
+    print("Buffer remaining", reminaingBuffer)
+    return reminaingBuffer
+
 
 class Job(threading.Thread):
 
-    def __init__(self, port, file, delay, *args, **kwargs):
+    def __init__(self, port, file, delay, line, *args, **kwargs):
         super(Job, self).__init__(*args, **kwargs)
         self.__flag = threading.Event()
         self.__flag.set()
@@ -47,9 +51,12 @@ class Job(threading.Thread):
         self.port = port
         self.file = file
         self.delay = delay
+        self.line = line
+        print(self.line)
 
     def run(self):
         global ser
+        global success
 
         print("opening file", self.file)
         lines = ""
@@ -58,28 +65,54 @@ class Job(threading.Thread):
         command_list = lines.split(";")
 
         line_list = list()
+
         for i in range(len(command_list)):
             line_list.append(command_list[i] + ";")
         line_list = [f.strip() for f in line_list]
 
         print("opening serial port", self.port)
-        ser = serial.Serial(self.port, 9600, timeout=0.5)
+        ser = serial.Serial(
+            self.port,
+            9600,
+            timeout=1.0,
+            inter_byte_timeout=self.delay
+        )
+        time.sleep(0.2)
+        ser.write(bytes("\033.P3;", "utf-8")) # Handshake
 
-        i = 0
+        i = self.line
+        print("START", i)
         print("LEN", len(line_list))
         while i < len(line_list):
-            print(i, line_list[i])
-            ser.write(bytes(line_list[i], "utf-8"))
-            time.sleep(len(line_list[i]) * 0.01)
-
-            i = i + 1;
+            if("SP" in line_list[i]):
+                print("----")
+                print(f"Selecting new pen ({line_list[i]}). Waiting for user confirmation")
+                print("Type \"r\" to continue")
+                print("----")
+                self.pause()
+                time.sleep(0.4)
 
             if(self.__running.isSet()):
                 self.__flag.wait()
 
+            delay = (len(line_list[i]) * self.delay)
+            # print(f"{i}\t {delay}\t {line_list[i]}")
 
-        ser.close()
+            ser.write(bytes(line_list[i], "utf-8"))
+            time.sleep(delay)
+
+            i = i + 1;
+
+            if(i % 10 == 0):
+                buffer = availabeBuffer()
+                # while buffer != 1024:
+                #     print("Waiting on Buffer")
+                #     time.sleep(1)
+                #     buffer = availabeBuffer()
+
+        # ser.close()
         self.stop()
+        success = True
 
     def pause(self):
         self.__flag.clear()
@@ -94,13 +127,16 @@ class Job(threading.Thread):
 
 def exitHandler():
     global ser
+    global success
     try:
-        ser.write(bytes("\033.J\r\n", "utf-8"))
-        ser.write(bytes("\033.K\r\n", "utf-8"))
-        ser.write(bytes("\033.Z\r\n", "utf-8"))
+        if not success:
+            ser.write(bytes("\033.J;", "utf-8"))
+            ser.write(bytes("\033.K;", "utf-8"))
+            ser.write(bytes("\033.Z;", "utf-8"))
+            print("--- ABORTING ---")
         ser.close()
     except: pass
-    print("Aborting")
+    print("END")
     return 0
 
 atexit.register(exitHandler)
@@ -123,7 +159,14 @@ parser.add_argument('-d', '--delay',
                     dest='delay',
                     help='Command delay',
                     type=float,
-                    default=0.05
+                    default=0.01
+                    )
+parser.add_argument('-l', '--line',
+                    action='store',
+                    dest='line',
+                    help='Line Start',
+                    type=int,
+                    default=0
                     )
 parser.add_argument('-v', '--version',
                     action='store_true',
@@ -143,7 +186,7 @@ if(args.get("port", None) is None):
     print("port cannot be undefined. Add using --port flag")
     sys.exit(0)
 
-a = Job(args.get("port"), args.get("file"), args.get("delay"))
+a = Job(args.get("port"), args.get("file"), args.get("delay"), args.get("line"))
 a.start()
 
 time.sleep(0.5)
@@ -164,7 +207,8 @@ while a.is_alive():
         elif("R" in prompt):
             print("resuming")
             a.resume()
-
+        elif("B" in prompt):
+            availabeBuffer()
 
 print("Job done - exiting")
 sys.exit(1)
